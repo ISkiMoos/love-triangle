183be9856d01466be94da799a8c56f96466e8a2d
ISkiMoos <alexandrglushkoff@gmail.com>
2019-03-17 14:21:49 +0300

    tree/feature: add function to find a love triangle

--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
 /**
- * @param preferences - an array of integers. Indices of people, whom they love
- * @returns number of love triangles
+ * @param preferences - an array of integers. Indices of people, whom they love  
+                        (массив целых чисел. показатели людей, которых они любят)
+ * 
+ * @returns number of love triangles (Количество любовных треугольников)
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    
+
+    let result = 0;
+
+    for(let i = 0; i < preferences.length; i++) {
+
+        let a = preferences[i] - 1;
+        if (a == i) continue;
+
+        let b = preferences[a] - 1;
+        if (b == a) continue;
+
+        let c = preferences[b] - 1;
+        if (c == b) continue;
+
+        if ( c == i) {
+            result++;
+        }
+    }
+    return result / 3;
+
 };
